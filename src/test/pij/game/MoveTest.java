ac2960264ba22115d8b4f0f590f2512e984e65e4
woodmancini <etwemmett@gmail.com>
2026-02-24 20:08:49 +0000

    Fixed/updated some tests.

--- src/test/pij/game/MoveTest.java
+++ src/test/pij/game/MoveTest.java
@@ -1,9 +1,14 @@
 package pij.game;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import pij.board.Board;
+import pij.board.BoardParser;
 import pij.board.Coordinate;
+import pij.exceptions.IllegalMoveException;
 import pij.tile.Tile;
 
 import java.util.ArrayList;
@@ -12,11 +17,49 @@ import java.util.List;
 
 public class MoveTest {
 
-//    @Test
-//    void testAppleMoveIsValidWord() {
-//        Move move = new Move(List.of(new Tile('A'), new Tile('P'), new Tile('P'), new Tile('L'), new Tile('E')), new Coordinate(5, 5), false);
-//        assertTrue(move.isValidWord());
-//    }
+    private GameRunner gameRunner;
+
+    @BeforeEach
+    void SetUp() {
+        gameRunner = new GameRunner();
+        gameRunner.setBoard(BoardParser.parseBoardFromFile());
+    }
+
+    @Test
+    void testMustUseStartSquare() {
+        assertThrows(IllegalMoveException.class, () -> gameRunner.validateMove(new Move(List.of(new Tile('D'), new Tile('I'), new Tile('N'), new Tile('E'), new Tile('D')),
+                new Coordinate(3, 7), true)));
+    }
+
+    @Test
+    void test_DINED_d4_Scores20() {
+        int actual = gameRunner.makeMove(new Move(List.of(new Tile('D'), new Tile('I'), new Tile('N'), new Tile('E'), new Tile('D')),
+                new Coordinate(3, 3), true));
+        int expected = 20;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testOutOfBoundsMove() {
+
+        assertThrows(IllegalMoveException.class, () -> gameRunner.validateMove(new Move(List.of(new Tile('D'), new Tile('I'), new Tile('N'), new Tile('E'), new Tile('D')),
+                new Coordinate(3, 7), true)));
+
+    }
+
+    @Test
+    void test_TNZON_7c_Scores19() throws IllegalMoveException {
+        char[] dined = "DINED".toCharArray();
+        gameRunner.makeMove(gameRunner.buildMove(dined, "d4"));
+        char[] tenzon = "TNZON".toCharArray();
+        int actual = gameRunner.makeMove(gameRunner.buildMove(tenzon, "7c"));
+        int expected = 19;
+        assertEquals(expected, actual);
+    }
+
+    //new Move(List.of(new Tile('D'), new Tile('I'), new Tile('N'), new Tile('E'), new Tile('D')
+
+
 //
 //    @Test
 //    void testStringToMoveValidVertical() {
--- src/test/pij/tile/TileTest.java
+++ src/test/pij/tile/TileTest.java
@@ -1,52 +1,66 @@
 package pij.tile;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pij.game.HumanPlayer;
+import pij.game.Player;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class TileTest {
 
-    private static List<Tile> tiles;
+    private TileBag tileBag;
+    private Map<Character, Integer> tileCount;
+    private Player testPlayer;
 
-    @BeforeAll
-    static void setUp() {
-        tiles = new TileBag().getContents();
+    @BeforeEach
+    void setUp() {
+        tileBag = new TileBag();
+        tileCount = new HashMap<>();
+        for (Tile tile: tileBag.getContents()) {
+            tileCount.put(tile.getLetter(), (tileCount.getOrDefault(tile.getLetter(), 0) + 1));
+        }
+        testPlayer = new HumanPlayer("testPlayer");
     }
 
     @Test
     void test100TilesInNewBag() {
         int expected = 100;
-        int actual = tiles.size();
+        int actual = tileBag.getContents().size();
         assertEquals(expected, actual);
     }
 
     @Test
     void testAllTypesOfTilePresent() {
-        var tileSet = new HashSet<>(tiles);
         int expected = 27;
-        int actual = tileSet.size();
+        int actual = tileCount.size();
         assertEquals(expected, actual);
     }
 
     @Test
     void test8ATiles() {
-        Map<Tile, Integer> tileCount = new HashMap<>();
-        for (Tile tile: tiles) {
-            tileCount.put(tile, (tileCount.getOrDefault(tile, 0) + 1));
-        }
-        System.out.println(tileCount);
         int expected = 8;
-//        int actual = tileCount.get(...);
-//        assertEquals(expected, actual);
+        int actual = tileCount.get('A');
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void test93TilesAfterDealingHand() {
+        tileBag.deal(testPlayer);
+        int expected = 93;
+        int actual = tileBag.getContents().size();
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void testPlayerHandContains7Tiles() {
+        tileBag.deal(testPlayer);
+        int expected = 7;
+        int actual = testPlayer.getHand().size();
+        assertEquals(expected, actual);
+    }
 
 }
